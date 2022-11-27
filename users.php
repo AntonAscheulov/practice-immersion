ac1fa4dd837b65a634703aef82a8f5a231022412
AntonAscheulov <schuca120@mail.ru>
2022-11-27 19:48:42 +0300

    user profile task done

--- users.php
+++ users.php
@@ -78,7 +78,9 @@ $users = get_users();
                         <div class="card-body border-faded border-top-0 border-left-0 border-right-0 rounded-top">
                             <div class="d-flex flex-row align-items-center">
                                 <span class="status status-<?php echo get_status($user)?> mr-3">
+                                    <a href="page_profile.php?id=<?php echo $user['id']?>">
                                     <span class="rounded-circle profile-image d-block " style="background-image:url(<?php echo get_avatar($user)?>); background-size: cover;"></span>
+                                    </a>
                                 </span>
                                 <div class="info-card-text flex-1">
                                     <a href="javascript:void(0);" class="fs-xl text-truncate text-truncate-lg text-info" data-toggle="dropdown" aria-expanded="false">
